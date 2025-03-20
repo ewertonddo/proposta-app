6dda2e569d54b5eccc6cb869a7f1fd2aa54c448b
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-03-20 16:07:32 -0300

    Criando Dtos

---
+++ src/main/java/com/ewerton/proposta_app/dto/PropostaResponseDto.java
@@ -0,0 +1,24 @@
+package com.ewerton.proposta_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PropostaResponseDto {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String cpf;
+    private Double renda;
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovado;
+    private String observacao;
+}
