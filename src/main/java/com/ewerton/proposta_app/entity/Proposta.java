a037b96a877ad040dae5955727aa8313f22430c7
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-03-20 15:28:37 -0300

    Criando Entidades

---
+++ src/main/java/com/ewerton/proposta_app/entity/Proposta.java
@@ -0,0 +1,21 @@
+package com.ewerton.proposta_app.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Proposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double valorSolicitado;
+    private int prazoPagamento;
+    private Boolean aprovado;
+    private boolean integrada;
+    private String observacao;
+
+    @OneToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+}
