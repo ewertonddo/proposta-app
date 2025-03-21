09653421f99a08c2c0a44b8aec8a8528da83b762
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-03-21 16:28:51 -0300

    Criando o Controler para a proposta

---
+++ src/main/java/com/ewerton/proposta_app/controller/PropostaController.java
@@ -0,0 +1,26 @@
+package com.ewerton.proposta_app.controller;
+
+import com.ewerton.proposta_app.dto.PropostaRequestDto;
+import com.ewerton.proposta_app.dto.PropostaResponseDto;
+import com.ewerton.proposta_app.service.PropostaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proposta")
+public class PropostaController {
+
+    @Autowired
+    private PropostaService service;
+
+    @PostMapping
+    public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto){
+        PropostaResponseDto response =  service.criar(requestDto);
+        return ResponseEntity.ok(response);
+    }
+
+}
