b6debf55e85e881a066feba8ee45aa32ad9a80e3
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-03-24 16:11:03 -0300

    Criando o Mapper para a proposta

---
+++ src/main/java/com/ewerton/proposta_app/mapper/PropostaMapper.java
@@ -0,0 +1,28 @@
+package com.ewerton.proposta_app.mapper;
+
+import com.ewerton.proposta_app.dto.PropostaRequestDto;
+import com.ewerton.proposta_app.dto.PropostaResponseDto;
+import com.ewerton.proposta_app.entity.Proposta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PropostaMapper {
+
+    PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
+
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.renda", source = "renda")
+    Proposta convertDtoToProposta(PropostaRequestDto request);
+
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "renda", source = "usuario.renda")
+    PropostaResponseDto convertPropostaToDto(Proposta proposta);
+}
