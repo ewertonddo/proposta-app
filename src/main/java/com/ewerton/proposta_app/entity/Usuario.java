6af7774c15884ef3d3353ad090655637717e0f9c
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-03-24 16:12:21 -0300

    Incluindo get e set para geracao do mapper

--- src/main/java/com/ewerton/proposta_app/entity/Usuario.java
+++ src/main/java/com/ewerton/proposta_app/entity/Usuario.java
@@ -1,7 +1,19 @@
 package com.ewerton.proposta_app.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Usuario {
 
