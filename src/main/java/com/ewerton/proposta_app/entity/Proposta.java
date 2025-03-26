4b64bccdaf811d85e28742bc3db388f6f538863e
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-03-26 15:04:01 -0300

    Ajustando imports e adicionando cascade na Entidade Proposta

--- src/main/java/com/ewerton/proposta_app/entity/Proposta.java
+++ src/main/java/com/ewerton/proposta_app/entity/Proposta.java
@@ -1,11 +1,6 @@
 package com.ewerton.proposta_app.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,7 +23,7 @@ public class Proposta {
     private boolean integrada;
     private String observacao;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 }
