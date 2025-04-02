c8358c96692d601137e5f60f4fe43efa79b9f32a
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-04-02 14:40:10 -0300

    Alterando tipo do objeto utilizado

--- src/main/java/com/ewerton/proposta_app/service/NotificacaoService.java
+++ src/main/java/com/ewerton/proposta_app/service/NotificacaoService.java
@@ -1,6 +1,6 @@
 package com.ewerton.proposta_app.service;
 
-import com.ewerton.proposta_app.dto.PropostaResponseDto;
+import com.ewerton.proposta_app.entity.Proposta;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,7 @@ public class NotificacaoService {
 
     private RabbitTemplate rabbitTemplate;
 
-    public void notificar(PropostaResponseDto response, String exchange){
-        rabbitTemplate.convertAndSend(exchange,"", response);
+    public void notificar(Proposta proposta, String exchange){
+        rabbitTemplate.convertAndSend(exchange,"", proposta);
     }
 }
