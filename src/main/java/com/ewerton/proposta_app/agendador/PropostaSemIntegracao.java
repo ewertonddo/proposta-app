5c68bd34e33927a7eb8ef05b449587fd549a3a10
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-04-03 14:34:10 -0300

    Adicionado schedule para realizar a chamada a cada 10 segundos

--- src/main/java/com/ewerton/proposta_app/agendador/PropostaSemIntegracao.java
+++ src/main/java/com/ewerton/proposta_app/agendador/PropostaSemIntegracao.java
@@ -1,14 +1,20 @@
 package com.ewerton.proposta_app.agendador;
 
+import com.ewerton.proposta_app.entity.Proposta;
 import com.ewerton.proposta_app.repository.PropostaRepository;
 import com.ewerton.proposta_app.service.NotificacaoService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class PropostaSemIntegracao {
 
-    private PropostaRepository repository;
+    private final PropostaRepository repository;
+    private final NotificacaoService notificacaoService;
+    private final String exchangePropostaPendente;
 
     public PropostaSemIntegracao(PropostaRepository repository,
                                  NotificacaoService notificacaoService,
@@ -18,17 +24,20 @@ public class PropostaSemIntegracao {
         this.exchangePropostaPendente = exchangePropostaPendente;
     }
 
-    private NotificacaoService notificacaoService;
-    private String exchangePropostaPendente;
-
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
     public void buscarPropostasSemIntegracao(){
         repository.findAllByIntegradaIsFalse().forEach(proposta -> {
             try {
                 notificacaoService.notificar(proposta, exchangePropostaPendente);
-                proposta.setIntegrada(true);
+                atualizarProposta(proposta);
             }catch(RuntimeException ex){
                 System.out.println(ex);
             }
         });
+    }
+
+    private void atualizarProposta(Proposta proposta) {
+        proposta.setIntegrada(true);
+        repository.save(proposta);
     }
 }
