3dd61f3ce8d7ada3f7290bc9e423c4bbed9bc9d3
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-04-08 15:09:05 -0300

    Gerando testes da classe PropostaController

---
+++ src/test/java/com/ewerton/proposta_app/controller/PropostaControllerTest.java
@@ -0,0 +1,85 @@
+package com.ewerton.proposta_app.controller;
+
+import com.ewerton.proposta_app.dto.PropostaRequestDto;
+import com.ewerton.proposta_app.dto.PropostaResponseDto;
+import com.ewerton.proposta_app.service.PropostaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class PropostaControllerTest {
+
+    public static final String NOME = "Jose";
+    public static final String SOBRENOME = "Silva";
+    public static final String TELEFONE = "11999999999";
+    public static final String CPF = "12345678909";
+    public static final double RENDA = 1000.0;
+    public static final String VALOR_SOLICITADO_FMT = "500";
+    public static final int PRAZO_PAGAMENTO = 12;
+    public static final boolean APROVADO = true;
+    public static final String OBS = "obs";
+    public static final double VALOR_SOLICITADO = 200.0;
+    public static final Long ID = Long.valueOf(0);
+
+    private PropostaResponseDto propostaResponseDto;
+    private PropostaRequestDto propostaRequestDto;
+
+    @InjectMocks
+    private PropostaController controller;
+
+    @Mock
+    private PropostaService service;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+        start();
+    }
+
+    @Test
+    void whenCriarReturnSuccess() {
+        Mockito.when(service.criar(Mockito.any())).thenReturn(propostaResponseDto);
+
+        ResponseEntity<PropostaResponseDto> response = controller.criar(propostaRequestDto);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(response.getBody().getId(), ID);
+    }
+
+    @Test
+    void whenObterPropostaReturnSuccess() {
+        Mockito.when(service.obterProposta()).thenReturn(List.of(propostaResponseDto));
+
+        ResponseEntity<List<PropostaResponseDto>> response = controller.obterProposta();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(PropostaResponseDto.class, response.getBody().get(0).getClass());
+    }
+
+    void start(){
+        propostaResponseDto = new PropostaResponseDto(ID, NOME
+                , SOBRENOME, TELEFONE, CPF
+                , RENDA, VALOR_SOLICITADO_FMT, PRAZO_PAGAMENTO
+                , APROVADO, OBS);
+
+        propostaRequestDto = new PropostaRequestDto(NOME, SOBRENOME, TELEFONE, CPF, RENDA
+                , VALOR_SOLICITADO, PRAZO_PAGAMENTO);
+    }
+}
+
