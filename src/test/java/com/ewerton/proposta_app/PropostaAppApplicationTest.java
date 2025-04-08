f0095b3f212191c86d3e9a16f127d7a64d479291
ewertonddo <ewertonoliveira.eu@gmail.com>
2025-04-08 15:08:06 -0300

    Gerando testes da classe PropostaAppApplication

---
+++ src/test/java/com/ewerton/proposta_app/PropostaAppApplicationTest.java
@@ -0,0 +1,15 @@
+package com.ewerton.proposta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PropostaAppApplicationTest {
+
+    @Test
+    void main() {
+        PropostaAppApplication.main(new String[] {});
+    }
+}
